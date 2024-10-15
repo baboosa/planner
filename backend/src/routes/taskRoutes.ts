26915d2eaae619583c9e67b11f9af8ddce13a9ce
weverton <weverton.wrb10@gmail.com>
2024-10-15 18:55:16 -0300

    feat: add getTaskById function to retrieve tasks from the database

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { addTask } from '../controllers/taskController'
+import { addTask, getTaskById } from '../controllers/taskController'
 import { validateTask } from '../middleware/validationMiddleware'
 
 const router = Router()
 
 // routes
 router.post('/tasks', validateTask, addTask)
+router.get('/tasks/:id', getTaskById)
 
 export default router
