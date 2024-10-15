7a6175e767c583ed03ff203c42a7e301b53e2ce5
weverton <weverton.wrb10@gmail.com>
2024-10-14 22:33:09 -0300

    feat: add taskController.ts

---
+++ backend/src/controllers/taskController.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface ITask extends Document {
+    title: string
+    description: string
+    completed: boolean
+}
+
+const TaskSchema: Schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+})
+
+export const Task = mongoose.model<ITask>('Task', TaskSchema);
