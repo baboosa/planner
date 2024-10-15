a33ab67e6d0bda372ebb32258cd5911292925554
weverton <weverton.wrb10@gmail.com>
2024-10-15 19:02:58 -0300

    feat: add getTaskById function to retrieve tasks from the database on taskController

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -1,23 +1,38 @@
-import { Request, Response } from 'express'
-import { Task } from '../models/taskModel'
+import { NextFunction, Request, Response } from 'express'
+import { ITask, Task } from '../models/taskModel'
 import { createClient } from 'redis'
 import { promisify } from 'util'
 
 // Inicializando o redis
-const redisClient = createClient({ url: process.env.REDIS_URL })
-const getCache = promisify(redisClient.get).bind(redisClient)
+//const redisClient = createClient({ url: process.env.REDIS_URL })
+//const getCache = promisify(redisClient.get).bind(redisClient)
 
 export const addTask = async (req: Request, res: Response) => {
-    const { title, description } = req.body;
+    const { title, description } = req.body
     try {
-        const task = new Task({ title, description });
-        await task.save();
+        const task = new Task({ title, description })
+        await task.save()
         
-        // Invalida o cache quando uma nova tarefa é adicionada
-        redisClient.del('tasks');
+        //redisClient.del('tasks')
 
-        res.status(201).json(task);
+        res.status(201).json(task)
     } catch (error) {
-        res.status(400).json({ error: (error as Error).message });
+        res.status(400).json({ error: (error as Error).message })
+    }
+}
+
+export const getTaskById = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+        const taskId = req.params.id
+        const task: ITask | null = await Task.findById(taskId)
+
+        if (!task) {
+            return res.status(404).json({error: 'Task not found'})
+        }
+        res.status(200).json(task)
+
+    } catch (error: unknown) {
+        console.log(error)
+        res.status(500).json({ error: 'Error fetching task' })
     }
 }
