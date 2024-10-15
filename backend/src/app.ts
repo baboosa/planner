2383c8be7ca898350f4c9a2584df791307405ca7
weverton <weverton.wrb10@gmail.com>
2024-10-14 22:59:03 -0300

    feat: add app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import taskRoutes from './routes/taskRoutes';
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+// Carregar variáveis de ambiente
+dotenv.config();
+
+const app = express();
+
+// Conectar ao MongoDB
+mongoose.connect(process.env.MONGODB_URI || '').then(() => {
+    console.log('Conectado ao MongoDB');
+}).catch((error) => {
+    console.error('Erro ao conectar ao MongoDB:', error);
+});
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rotas
+app.use('/api', taskRoutes);
+
+export default app;
