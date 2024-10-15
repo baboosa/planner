0c4dbdeda2717a87fd4f95ed0ff9448033ceaac1
weverton <weverton.wrb10@gmail.com>
2024-10-15 00:14:09 -0300

    feat(routes): add route for adding a new task

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
+import { addTask } from '../controllers/taskController'
+import { validateTask } from '../middleware/validationMiddleware'
 
 const router = Router()
 
 // routes
+router.post('/tasks', validateTask, addTask)
 
 export default router
