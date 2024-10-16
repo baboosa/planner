d9d7b13ee016189d3297af5ac767eda2f0f6a1f5
weverton <weverton.wrb10@gmail.com>
2024-10-16 12:24:50 -0300

    refactor: Remove trailing semicolons

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,33 +1,33 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import taskRoutes from './routes/taskRoutes';
+import express from 'express'
+import mongoose from 'mongoose'
+import taskRoutes from './routes/taskRoutes'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import authRoutes from './routes/authRoutes';
-import { sessionMiddleware } from './config/session';
+import authRoutes from './routes/authRoutes'
+import { sessionMiddleware } from './config/session'
 
 // Carregar variáveis de ambiente
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
 // Conectar ao MongoDB
 mongoose.connect(process.env.MONGODB_URI || '').then(() => {
-    console.log('Conectado ao MongoDB');
+    console.log('Conectado ao MongoDB')
 }).catch((error) => {
-    console.error('Erro ao conectar ao MongoDB:', error);
-});
+    console.error('Erro ao conectar ao MongoDB:', error)
+})
 
 // Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(sessionMiddleware); // Usar sessões
+app.use(cors())
+app.use(express.json())
+app.use(sessionMiddleware) // Usar sessões
 
 
 // Rotas
-app.use('/', taskRoutes);
-app.use('/auth', authRoutes);
+app.use('/', taskRoutes)
+app.use('/auth', authRoutes)
 
-app.use(express.json());
+app.use(express.json())
 
-export default app;
+export default app
