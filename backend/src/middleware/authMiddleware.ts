b130991667f286ef2158258c94881a00677ef6fc
weverton <weverton.wrb10@gmail.com>
2024-10-16 11:58:19 -0300

    feat: add auth middlweare for authenticated user

---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+
+// Middleware para verificar se o usuário está logado
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.session.id) {
+        return res.status(401).json({ message: 'Acesso não autorizado, faça login.' })
+    }
+    next()
+}
